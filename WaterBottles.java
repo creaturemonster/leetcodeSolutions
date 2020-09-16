38655ab6346914a0b9201292e21a9033671461c3
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-16 16:00:09 -0400

    Create WaterBottles.java

---
+++ WaterBottles.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans=0;
+        int empty=0;
+        
+        while(numBottles > 0){
+            ans+=numBottles;
+            empty += numBottles;
+            numBottles = empty/ numExchange;
+            empty -= numBottles * numExchange;
+        }
+        return ans;
+    }
+}
+
+
+            
+          

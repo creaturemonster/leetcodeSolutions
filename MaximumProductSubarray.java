741babd32a223e9fd7e32368a12b9c5371369716
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-17 22:10:50 -0400

    Create MaximumProductSubarray.java

---
+++ MaximumProductSubarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int length=nums.length;
+        
+        //Base Case
+        if(length == 0){
+            return 0;
+        }
+        
+        //Set Variables
+        int min=nums[0];
+        int max=nums[0];
+        int result=nums[0];
+        
+        for(int i=1;i<length;i++){
+            int temp_max=Math.max(nums[i] * max, Math.max(nums[i] * min, nums[i]));
+            min=Math.min(nums[i]*max, Math.min(nums[i] * min, nums[i]));
+            
+            max=temp_max;
+            result=Math.max(result, max);
+        }
+        return result;
+    }
+}

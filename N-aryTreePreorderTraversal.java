0092ce7e8832d4600eff64f164c49ff6ad5419d7
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-29 17:44:24 -0400

    Create N-aryTreePreorderTraversal.java
    This is the recursive solution to Leetcode #589 N-ary Preorder Traversal

---
+++ N-aryTreePreorderTraversal.java
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        LinkedList<Integer>list=new LinkedList<>();
+        return helper(root, list);
+    }
+    public List<Integer> helper(Node root, LinkedList<Integer> list){
+        //Base case
+        if(root==null){
+            return list;
+        }
+        list.add(root.val);
+        for(Node child:root.children){
+            helper(child, list);
+        }
+        return list;
+        
+    }
+}
